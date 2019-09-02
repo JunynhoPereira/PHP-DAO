317764ec6cc3fb50ec1f43cdb4a78c7fef591ddc
JunynhoPereira <junynho.500@gmail.com>
2019-09-02 15:54:47 -0300

    Criação de modelos de busca

--- index.php
+++ index.php
@@ -2,9 +2,45 @@
 
 require_once("config.php");
 
+/*
+
+Buscar usuario usando o login e senha corretos
+
+$usuario = new Usuario();
+$usuario->login("Junynho","@12345");
+echo $usuario;
+
+*/
+
+
+/*
+
+Busca todos os usuarios que tenha informações do deslogin parecido, usado comando LIKE na classe search
+
+$search = Usuario::search("o");
+echo json_encode($search);
+
+*/
+
+/*
+
+Buscar todos os usuario em ordem alfabetica por conta da classe getList
+
+$lista = Usuario::getList();
+echo json_encode($lista);
+
+*/
+
+/* 
+
+Buscar o usuario que tenha o idcadastro igual ao loadById(??)
+
 $user = new Usuario();
 $user->loadById(37);
 echo $user;
+
+
+*/
 
 /*
 
