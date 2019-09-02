f1c3068ed5e834efe9634df55b0236af24b885ca
JunynhoPereira <junynho.500@gmail.com>
2019-09-02 15:24:06 -0300

    Criação da classe Usuario

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 
 require_once("config.php");
 
+$user = new Usuario();
+$user->loadById(37);
+echo $user;
+
+/*
+
+Buscar todos os dados da tabela tb_usuarios
+
 $sql = new Sql();
-
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($usuarios);
 
+*/
 ?>
